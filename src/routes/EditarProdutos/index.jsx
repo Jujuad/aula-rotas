fc3f3c332fb2286cfe1c1a3658fa957e77fc0f8f
julia.andrade-at-538590928221 <qoTQt0Cryp6/ysKmwHlONj6KyLQNdpMjVwl0qeqlNns=>
2023-09-07 04:43:08 -0300

    EditarProdutos - index /  passando valores pela uri

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,8 +1,25 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { listaProdutos } from "../../data/listarProdutos";
+
 export default function EditarProduto(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0]
+
+    const salvar = ()=>{
+        alert('Produto: ${produto.nome} editado com sucesso!')
+        return navegacao('/produtos')
+    }
 
     return(
         <main>
             <hi>Editando o produto</hi>
+            <p>Editando os dados do produto: {produto.nome}</p>
+            <button onClick={salvar}>Salvar</button>
         </main>
     )
 }
--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import listaProdutos from "../../data/listarProdutos";
+import { listaProdutos } from "../../data/listarProdutos";
 
 export default function Produtos(){
 
