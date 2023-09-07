a6b3d106417e005f6894c54bfe971babe8e7716f
julia.andrade-at-538590928221 <qoTQt0Cryp6/ysKmwHlONj6KyLQNdpMjVwl0qeqlNns=>
2023-09-07 04:32:55 -0300

    Produtos - index /  passando valores pela uri

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import listaProdutos from "../../data/listarProdutos";
+
 export default function Produtos(){
 
     return(
         <main>
             <hi>Produtos</hi>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}> 
+                    <Link to={'/produtos/editar/${prod.id}'}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
     )
 }
